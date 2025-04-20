4e6afb9c1370d0629e5fcc148d8196600b055ef6
nguyen <nguyenhainguyen2606@gmail.com>
2025-04-20 09:58:22 +0200

    update unit tests for project

---
+++ tests/test_chatbot_service.py
@@ -0,0 +1,10 @@
+# from src.services.chat_bot_service import ChatBotService
+# import pytest
+# # Enable when needed, this will cost tokens to run
+# @pytest.mark.asyncio
+# class TestChatBotService:
+#     chat_bot_service = ChatBotService("test_user")
+#
+#     async def test_achat(self):
+#         response = await self.chat_bot_service.achat("Hello")
+#         assert isinstance(response, str)
