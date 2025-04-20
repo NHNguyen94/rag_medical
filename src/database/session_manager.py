188cfd21b46ed66e47ba6757382337787fcbe5d2
nguyen <nguyenhainguyen2606@gmail.com>
2025-04-20 10:40:17 +0200

    update async engine to db

--- src/database/session_manager.py
+++ src/database/session_manager.py
@@ -1,7 +1,7 @@
 import os
 
 import dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_async_engine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
@@ -11,7 +11,12 @@ dotenv.load_dotenv()
 class SessionManager:
     # https://sqlmodel.tiangolo.com/tutorial/fastapi/session-with-dependency/#use-the-dependency
     def __init__(self):
-        self.engine = create_engine(os.getenv("DATABASE_URL"))
+        # self.engine = create_engine(os.getenv("DATABASE_URL"))
+        self.engine = create_async_engine(
+            os.getenv("DATABASE_URL"),
+            future=True,
+            # echo=True,
+        )
         self.Session = sessionmaker(bind=self.engine)
         self.AsyncSession = sessionmaker(
             bind=self.engine, class_=AsyncSession, expire_on_commit=False
