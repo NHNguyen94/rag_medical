dae98afc4b3085b38428943cc622b650e0720429
nguyen <nguyenhainguyen2606@gmail.com>
2025-07-11 05:56:33 +0200

    add eval pipeline

---
+++ src/pipelines/emotion_recognition/evaluate.py
@@ -0,0 +1,32 @@
+from typing import Dict
+
+from src.services.emotion_recognition_service import EmotionRecognitionService
+
+
+def run_eval(model_path: str, test_data_path: str) -> Dict:
+    emotion_recognition_service = EmotionRecognitionService(
+        test_data_path=test_data_path,
+    )
+    model, vocab = emotion_recognition_service.load_model(
+        model_path=model_path,
+    )
+    eval_result = emotion_recognition_service.evaluate(
+        model=model,
+        vocab=vocab,
+    )
+    return eval_result
+
+
+def main(
+        model_path: str,
+        test_data_path: str,
+) -> None:
+    eval_result = run_eval(model_path, test_data_path)
+    print(f"Evaluation Result: {eval_result}")
+
+
+if __name__ == "__main__":
+    model_path = "src/ml_models/model_files/cnn_model.pth"
+    test_data_path = "src/data/emotion_data/test.csv"
+
+    main(model_path, test_data_path)
---
+++ src/pipelines/topic_clustering/evaluate.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+from src.services.topic_clustering_service import TopicClusteringService
+
+
+def run_eval(test_data_path: str, model_path: str) -> Dict:
+    topic_clustering_service = TopicClusteringService(
+        test_data_path=test_data_path
+    )
+    model = topic_clustering_service.load_model(model_path)
+    eval_result = topic_clustering_service.evaluate(model)
+    return eval_result
+
+def main(test_data_path: str, model_path: str) -> None:
+    eval_result = run_eval(test_data_path, model_path)
+    print(f"Evaluation Result: {eval_result}")
+
+
+if __name__ == "__main__":
+    test_data_path = "src/data/medical_data/all/test.csv"
+    model_path = "src/ml_models/model_files/topic_clustering_model.pth"
+
+    main(test_data_path, model_path)
