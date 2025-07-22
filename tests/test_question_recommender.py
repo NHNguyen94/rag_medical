8ef4b58e8559f157b4fcff450fcf884e62d6c3f4
Jiso Chacko <jisochacko007@gmail.com>
2025-07-22 05:19:15 +0200

    Updated question recommendation tests

--- tests/test_question_recommender.py
+++ tests/test_question_recommender.py
@@ -1,19 +1,20 @@
-# from src.ml_models.flan_t5 import FlanT5
-# from src.services.question_service import QuestionService
-#
-#
-# class TestQuestionRecommender:
-#     model = FlanT5()
-#     service = QuestionService()
-#
-#     def test_question_recommender(self):
-#         question = "How is cancer diagnosed?"
-#         output = self.model.recommend(question, "Cancer")
-#         print(f"response1: {output} ")
-#         assert isinstance(output, list)
-#
-#     def test_question_service(self):
-#         question = "How is cancer diagnosed?"
-#         follow_up = self.service.get_follow_up_question(question, domain="Cancer")
-#         print(f"response2: {follow_up} ")
-#         assert isinstance(follow_up, list)
+from src.ml_models.flan_t5 import FlanT5
+from src.services.question_service import QuestionService
+
+
+qr_service = QuestionService()
+class TestQuestionRecommender:
+
+    def test_cancer_question_recommender(self):
+        model = qr_service.load_model(0)
+        question = "What are the causes cancer?"
+        output = qr_service.predict(question, model)
+        print(f"response1: {output} ")
+        assert isinstance(output, list)
+
+    def test_diabetes_question_recommender(self):
+        model = qr_service.load_model(1)
+        question = "What are the causes diabetes?"
+        output = qr_service.predict(question, model)
+        print(f"response1: {output} ")
+        assert isinstance(output, list)
